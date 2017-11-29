be43bc9b943f5c54a5f2da1fad10db2cb04b0f4f
Alex Truong <alexbt1992@csu.fullerton.edu>
2017-11-28 19:59:48 -0800

    how about now....

--- Code/interpreter.cpp
+++ Code/interpreter.cpp
@@ -12,172 +12,10 @@ Interpreter::Interpreter() {
 	binMap1 = {};									// Use 2 bitMaps to separate binary conversion into 2 binary-converted fields, ex:
 	binMap2 = {};									// 1A022DFE -> 1A02: Map1 = 0001101000000010 and 2DFE: Map2 = 0010110111111110
 	outFile.open("new_test_data.log", ios_base::app);
+	ptr = dataList.front();
 }
 
 Interpreter::~Interpreter() {}
-
-void Interpreter::list_of_data(list<MemoryData*> dataList) {
-	ptr = dataList.front();
-	while (true) {
-		// INITIALIZE FIRST NODE DATA
-		list_address = data.getAddress();
-		list_data = data.getData();
-		list_line_number = data.getLineNumber;
-		list_time = data.getTime();
-		
-		// IF FIRST NODE ADDRESS IS COMMAND CALL AND WORD DATA IS ZERO
-		if ((list_data == "00000000") && ((list_address == "40000810") || (list_address == "40000C18"))) {
-			message_output_rw(list_line_number, 0);
-			dataList.pop_front();
-			ptr = dataList.front();
-
-			// INCREMENT NODE
-			list_data = data.getData();
-			list_address = data.getAddress();
-			list_line_number = data.getLineNumber();
-		}
-
-		// IF FIRST NODE ADDRESS IS COMMAND CALL AND WORD DATA IS NOT ZERO
-		else if ((list_data != "00000000") && ((list_address == "40000810") || (list_address == "40000C18"))) {
-			// CONVERT HEX TO DECIMAL FOR WORD TOTAL
-			stringstream stream;
-			stream << list_data;
-			stream >> hex >> wordTotal;
-
-			// OUTPUT LINE NUMBER, COMMAND CALL, AND WORD TOTAL
-			message_output_rw(list_line_number, wordTotal);
-
-			// POP COMMAND CALL NODE AND MOVE TO ACTUAL DATA
-			dataList.pop_front();
-			ptr = dataList.front();
-
-			// INITIALIZE ADDRESS, BINARY, AND LINE NUMBER OF CURRENT NODE
-			list_address = data.getAddress();
-			list_data = data.getData();
-			list_line_number = data.getLineNumber();
-			binary = hex_to_bin(list_data);
-
-			// POP CURRENT NODE TO COMPARE ADDRESS WITH NEXT NODE
-			dataList.pop_front();
-			ptr = dataList.front();
-			list_address_2 = data.getAddress();
-
-			if ((list_address >= "40000818") && (list_address <= "4000086B")) {
-				if (list_address < list_address_2) {
-					bin_parser_inorder(binary, 0, wordTotal, lineNum);
-					wordCount = 2;
-
-					while (list_address <= "4000086B") {
-						list_address = data.getAddress();
-						list_data = data.getData();
-						binary = hex_to_bin(list_data);
-						list_line_number = data.getLineNumber();
-
-						bin_parser_inorder(binary, wordCount, wordTotal, lineNum);
-						dataList.pop_front();
-						ptr = dataList.front();
-					}
-				}
-				else if (list_address > list_address_2) {
-					wordCount = wordTotal;
-					bin_parser_revorder(binary, wordCount, lineNum);
-
-					while (list_address >= "40000818") {
-						list_address = data.getAddress();
-						list_data = data.getData();
-						binary = hex_to_bin(list_data);
-						list_line_number = data.getLineNumber();
-
-						bin_parser_revorder(binary, wordCount, lineNum);
-						dataList.pop_front();
-						ptr = dataList.front();
-					}
-				}
-			}
-			else if ((list_address >= "40000C20") && (list_address <= "40000C73")) {
-				if (list_address < list_address_2) {
-					bin_parser_inorder(binary, 0, wordTotal, lineNum);
-					wordCount = 2;
-
-					while (list_address <= "40000C73") {
-						list_address = data.getAddress();
-						list_data = data.getData();
-						binary = hex_to_bin(list_data);
-						list_line_number = data.getLineNumber();
-
-						bin_parser_inorder(binary, wordCount, wordTotal, lineNum);
-						dataList.pop_front();
-						ptr = dataList.front();
-					}
-				}
-				else if (list_address > list_address_2) {
-					wordCount = wordTotal;
-					bin_parser_revorder(binary, wordCount, lineNum);
-
-					while (list_address >= "40000C20") {
-						list_address = data.getAddress();
-						list_data = data.getData();
-						binary = hex_to_bin(list_data);
-						list_line_number = data.getLineNumber();
-
-						bin_parser_revorder(binary, wordCount, lineNum);
-						dataList.pop_front();
-						ptr = dataList.front();
-					}
-				}
-			}
-		}
-	}
-}
-
-// Hex to Binary Conversion
-#pragma region
-string Interpreter::hex_to_bin(string hexadecimal)
-{
-	string bin = "";
-	for (int i = 0; i < hexadecimal.length(); i++)
-	{
-		switch (hexadecimal.at(i))
-		{
-		case '0': bin.append("0000");
-			break;
-		case '1': bin.append("0001");
-			break;
-		case '2': bin.append("0010");
-			break;
-		case '3': bin.append("0011");
-			break;
-		case '4': bin.append("0100");
-			break;
-		case '5': bin.append("0101");
-			break;
-		case '6': bin.append("0110");
-			break;
-		case '7': bin.append("0111");
-			break;
-		case '8': bin.append("1000");
-			break;
-		case '9': bin.append("1001");
-			break;
-		case 'A': bin.append("1010");
-			break;
-		case 'B': bin.append("1011");
-			break;
-		case 'C': bin.append("1100");
-			break;
-		case 'D': bin.append("1101");
-			break;
-		case 'E': bin.append("1110");
-			break;
-		case 'F': bin.append("1111");
-			break;
-		default:
-			break;
-		}
-	}
-	return bin;
-}
-#pragma endregion
 
 // If the addresses are in ascending value order, then output in order
 int Interpreter::bin_parser_inorder(string binary, int wordCount, int wordTotal, int lineNum) {
@@ -361,3 +199,167 @@ void Interpreter::message_output_rw(int lineNum, int wordNum) {
 	else
 		outFile << "Line " << lineNum << ": <<RD/WR StoD/DtoS>> command: " << wordNum << " words" << endl;
 }
+
+void Interpreter::list_of_data(list<MemoryData*> dataList) {
+	ptr = dataList.front();
+	while (true) {
+		// INITIALIZE FIRST NODE DATA
+		list_address = data.getAddress();
+		list_data = data.getData();
+		list_line_number = data.getLineNumber;
+		list_time = data.getTime();
+		
+		// IF FIRST NODE ADDRESS IS COMMAND CALL AND WORD DATA IS ZERO
+		if ((list_data == "00000000") && ((list_address == "40000810") || (list_address == "40000C18"))) {
+			message_output_rw(list_line_number, 0);
+			dataList.pop_front();
+			ptr = dataList.front();
+
+			// INCREMENT NODE
+			list_data = data.getData();
+			list_address = data.getAddress();
+			list_line_number = data.getLineNumber();
+		}
+
+		// IF FIRST NODE ADDRESS IS COMMAND CALL AND WORD DATA IS NOT ZERO
+		else if ((list_data != "00000000") && ((list_address == "40000810") || (list_address == "40000C18"))) {
+			// CONVERT HEX TO DECIMAL FOR WORD TOTAL
+			stringstream stream;
+			stream << list_data;
+			stream >> hex >> wordTotal;
+
+			// OUTPUT LINE NUMBER, COMMAND CALL, AND WORD TOTAL
+			message_output_rw(list_line_number, wordTotal);
+
+			// POP COMMAND CALL NODE AND MOVE TO ACTUAL DATA
+			dataList.pop_front();
+			ptr = dataList.front();
+
+			// INITIALIZE ADDRESS, BINARY, AND LINE NUMBER OF CURRENT NODE
+			list_address = data.getAddress();
+			list_data = data.getData();
+			list_line_number = data.getLineNumber();
+			binary = hex_to_bin(list_data);
+
+			// POP CURRENT NODE TO COMPARE ADDRESS WITH NEXT NODE
+			dataList.pop_front();
+			ptr = dataList.front();
+			list_address_2 = data.getAddress();
+
+			if ((list_address >= "40000818") && (list_address <= "4000086B")) {
+				if (list_address < list_address_2) {
+					bin_parser_inorder(binary, 0, wordTotal, lineNum);
+					wordCount = 2;
+
+					while (list_address <= "4000086B") {
+						list_address = data.getAddress();
+						list_data = data.getData();
+						binary = hex_to_bin(list_data);
+						list_line_number = data.getLineNumber();
+
+						bin_parser_inorder(binary, wordCount, wordTotal, lineNum);
+						dataList.pop_front();
+						ptr = dataList.front();
+					}
+				}
+				else if (list_address > list_address_2) {
+					wordCount = wordTotal;
+					bin_parser_revorder(binary, wordCount, lineNum);
+
+					while (list_address >= "40000818") {
+						list_address = data.getAddress();
+						list_data = data.getData();
+						binary = hex_to_bin(list_data);
+						list_line_number = data.getLineNumber();
+
+						bin_parser_revorder(binary, wordCount, lineNum);
+						dataList.pop_front();
+						ptr = dataList.front();
+					}
+				}
+			}
+			else if ((list_address >= "40000C20") && (list_address <= "40000C73")) {
+				if (list_address < list_address_2) {
+					bin_parser_inorder(binary, 0, wordTotal, lineNum);
+					wordCount = 2;
+
+					while (list_address <= "40000C73") {
+						list_address = data.getAddress();
+						list_data = data.getData();
+						binary = hex_to_bin(list_data);
+						list_line_number = data.getLineNumber();
+
+						bin_parser_inorder(binary, wordCount, wordTotal, lineNum);
+						dataList.pop_front();
+						ptr = dataList.front();
+					}
+				}
+				else if (list_address > list_address_2) {
+					wordCount = wordTotal;
+					bin_parser_revorder(binary, wordCount, lineNum);
+
+					while (list_address >= "40000C20") {
+						list_address = data.getAddress();
+						list_data = data.getData();
+						binary = hex_to_bin(list_data);
+						list_line_number = data.getLineNumber();
+
+						bin_parser_revorder(binary, wordCount, lineNum);
+						dataList.pop_front();
+						ptr = dataList.front();
+					}
+				}
+			}
+		}
+	}
+}
+
+// Hex to Binary Conversion
+#pragma region
+string Interpreter::hex_to_bin(string hexadecimal)
+{
+	string bin = "";
+	for (int i = 0; i < hexadecimal.length(); i++)
+	{
+		switch (hexadecimal.at(i))
+		{
+		case '0': bin.append("0000");
+			break;
+		case '1': bin.append("0001");
+			break;
+		case '2': bin.append("0010");
+			break;
+		case '3': bin.append("0011");
+			break;
+		case '4': bin.append("0100");
+			break;
+		case '5': bin.append("0101");
+			break;
+		case '6': bin.append("0110");
+			break;
+		case '7': bin.append("0111");
+			break;
+		case '8': bin.append("1000");
+			break;
+		case '9': bin.append("1001");
+			break;
+		case 'A': bin.append("1010");
+			break;
+		case 'B': bin.append("1011");
+			break;
+		case 'C': bin.append("1100");
+			break;
+		case 'D': bin.append("1101");
+			break;
+		case 'E': bin.append("1110");
+			break;
+		case 'F': bin.append("1111");
+			break;
+		default:
+			break;
+		}
+	}
+	return bin;
+}
+#pragma endregion
+
