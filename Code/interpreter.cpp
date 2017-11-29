3296035d2b4aaf8ea157d31974b1ff7236752eb6
Alex Truong <alexbt1992@csu.fullerton.edu>
2017-11-28 19:45:28 -0800

    ALMOST DONE

--- Code/interpreter.cpp
+++ Code/interpreter.cpp
@@ -1,10 +1,12 @@
 #include "interpreter.h"
 #include <iostream>
+#include <sstream>
 
 Interpreter::Interpreter() {
 	binary = "";									//	Assign binary conversion from hex to bin cpp
 	outMsg = "";									//	Output message
 	wordCount = 0;									//	Track number of words from from parser
+	wordTotal = 0;
 	i = 0;
 	j = 0;
 	binMap1 = {};									// Use 2 bitMaps to separate binary conversion into 2 binary-converted fields, ex:
@@ -14,11 +16,139 @@ Interpreter::Interpreter() {
 
 Interpreter::~Interpreter() {}
 
+void Interpreter::list_of_data(list<MemoryData*> dataList) {
+	ptr = dataList.front();
+	while (true) {
+		// INITIALIZE FIRST NODE DATA
+		list_address = data.getAddress();
+		list_data = data.getData();
+		list_line_number = data.getLineNumber;
+		list_time = data.getTime();
+		
+		// IF FIRST NODE ADDRESS IS COMMAND CALL AND WORD DATA IS ZERO
+		if ((list_data == "00000000") && ((list_address == "40000810") || (list_address == "40000C18"))) {
+			message_output_rw(list_line_number, 0);
+			dataList.pop_front();
+			ptr = dataList.front();
+
+			// INCREMENT NODE
+			list_data = data.getData();
+			list_address = data.getAddress();
+			list_line_number = data.getLineNumber();
+		}
+
+		// IF FIRST NODE ADDRESS IS COMMAND CALL AND WORD DATA IS NOT ZERO
+		else if ((list_data != "00000000") && ((list_address == "40000810") || (list_address == "40000C18"))) {
+			// CONVERT HEX TO DECIMAL FOR WORD TOTAL
+			stringstream stream;
+			stream << list_data;
+			stream >> hex >> wordTotal;
+
+			// OUTPUT LINE NUMBER, COMMAND CALL, AND WORD TOTAL
+			message_output_rw(list_line_number, wordTotal);
+
+			// POP COMMAND CALL NODE AND MOVE TO ACTUAL DATA
+			dataList.pop_front();
+			ptr = dataList.front();
+
+			// INITIALIZE ADDRESS, BINARY, AND LINE NUMBER OF CURRENT NODE
+			list_address = data.getAddress();	
+			list_data = data.getData();			
+			list_line_number = data.getLineNumber();
+			binary = hex_to_bin(list_data);
+
+			// POP CURRENT NODE TO COMPARE ADDRESS WITH NEXT NODE
+			dataList.pop_front();				
+			ptr = dataList.front();				
+			list_address_2 = data.getAddress();
+
+			if (list_address < list_address_2) {
+				bin_parser_inorder(binary, 0, wordTotal, lineNum);
+				wordCount = 2;
+
+				while (list_address <= "4000086B") {
+					list_address = data.getAddress();
+					list_data = data.getData();
+					binary = hex_to_bin(list_data);
+					list_line_number = data.getLineNumber();
+
+					bin_parser_inorder(binary, wordCount, wordTotal, lineNum);
+					dataList.pop_front();
+					ptr = dataList.front();
+				}
+			}
+			else if (list_address > list_address_2) {
+				wordCount = wordTotal;
+				bin_parser_revorder(binary, wordCount, lineNum);
+				
+				while (list_address >= "40000818") {
+					list_address = data.getAddress();
+					list_data = data.getData();
+					binary = hex_to_bin(list_data);
+					list_line_number = data.getLineNumber();
+
+					bin_parser_revorder(binary, wordCount, lineNum);
+					dataList.pop_front();
+					ptr = dataList.front();
+				}
+			}
+		}
+	}
+}
+
+// Hex to Binary Conversion
+#pragma region
+string Interpreter::hex_to_bin(string hexadecimal)
+{
+	string bin = "";
+	for (int i = 0; i < hexadecimal.length(); i++)
+	{
+		switch (hexadecimal.at(i))
+		{
+		case '0': bin.append("0000");
+			break;
+		case '1': bin.append("0001");
+			break;
+		case '2': bin.append("0010");
+			break;
+		case '3': bin.append("0011");
+			break;
+		case '4': bin.append("0100");
+			break;
+		case '5': bin.append("0101");
+			break;
+		case '6': bin.append("0110");
+			break;
+		case '7': bin.append("0111");
+			break;
+		case '8': bin.append("1000");
+			break;
+		case '9': bin.append("1001");
+			break;
+		case 'A': bin.append("1010");
+			break;
+		case 'B': bin.append("1011");
+			break;
+		case 'C': bin.append("1100");
+			break;
+		case 'D': bin.append("1101");
+			break;
+		case 'E': bin.append("1110");
+			break;
+		case 'F': bin.append("1111");
+			break;
+		default:
+			break;
+		}
+	}
+	return bin;
+}
+#pragma endregion
+
 // If the addresses are in ascending value order, then output in order
-int Interpreter::bin_parser_inorder(string binary, int wordCount, int wordTotal) {
+int Interpreter::bin_parser_inorder(string binary, int wordCount, int wordTotal, int lineNum) {
 	while (true) {
 		int x = wordCount;
-		int lineNum = 0;
 
 		for (i = 15, j = 0; i > -1; i--, j++) {
 			binMap1[i] = binary.at(j);
@@ -46,10 +176,9 @@ int Interpreter::bin_parser_inorder(string binary, int wordCount, int wordTotal)
 }
 
 // If the addresses are in descending value order, then output in reverse order
-int Interpreter::bin_parser_revorder(string binary, int wordCount) {
+int Interpreter::bin_parser_revorder(string binary, int wordCount, int lineNum) {
 	while (true) {
 		int x = wordCount;
-		int lineNum = 0;
 
 		for (i = 15, j = 16; i > -1; i--, j++) {
 			binMap2[i] = binary.at(j);
@@ -76,6 +205,7 @@ int Interpreter::bin_parser_revorder(string binary, int wordCount) {
 }
 
 // Output messages depending on word number, see assignment
+#pragma region
 string Interpreter::bin_to_message(int wordCount, map<int, char> binMap) {
 	switch (wordCount) {
 	case 0: {
@@ -182,6 +312,7 @@ string Interpreter::bin_to_message(int wordCount, map<int, char> binMap) {
 	}
 	}
 }
+#pragma endregion
 
 // Output formatting depending on line number, word number, field names, and descriptions
 void Interpreter::message_output_fields(int lineNum, int wordNum, string message) {
--- Code/interpreter.h
+++ Code/interpreter.h
@@ -4,20 +4,28 @@
 #include <string>
 #include <map>
 #include <fstream>
+#include <list>
+#include "memorydata.h"
+#include "field_name_commands.h"
 using namespace std;
 
 class Interpreter {
 public:
 	Interpreter();
 	~Interpreter();
-	int bin_parser_inorder(string, int, int);
-	int bin_parser_revorder(string, int);
-	string bin_to_message(int, map<int, char>);
+	int bin_parser_inorder(string, int, int, int);
+	int bin_parser_revorder(string, int, int);
+	void list_of_data(list<MemoryData*>);
 	void message_output_rw(int, int);
 
 private:
-	char* binary;
+	string binary;
 	string outMsg;
+	string list_address;
+	string list_address_2;
+	string list_data;
+	string list_time;
+	int list_line_number;
 	int wordCount;
 	int wordTotal;
 	int i;
@@ -25,7 +33,9 @@ private:
 	map<int, char> binMap1;
 	map<int, char> binMap2;
 	ofstream outFile;
+	MemoryData data;
 	void message_output_fields(int, int, string);
+	string hex_to_bin(string);
 };
 
 #endif
