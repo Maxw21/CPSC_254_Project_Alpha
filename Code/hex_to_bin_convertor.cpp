284feeb3672574afa4d83a02252aadf058fe3520
admin <kirensyed@sbcglobal.net>
2017-11-28 16:22:55 -0800

    imporved hex to bin convertor

--- Code/hex_to_bin_convertor.cpp
+++ Code/hex_to_bin_convertor.cpp
@@ -1,56 +1,58 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-void hex_to_bin(char* hexadecimal);
+string hex_to_bin(string hexadecimal);
 int main()
 {
 	hex_to_bin("1A02");
 	system("pause");
 }
-void hex_to_bin(char* hexadecimal)
+string hex_to_bin(string hexadecimal)
 {
-	int i = 0;
-	while (hexadecimal[i])
+
+	string bin = "";
+	for (int i = 0; i < hexadecimal.length(); i++)
 	{
-		switch (hexadecimal[i])
+		switch (hexadecimal.at(i))
 		{
-		case '0': cout << "0000";
+		case '0': bin.append("0000");
 			break;
-		case '1': cout << "0001";
+		case '1': bin.append("0001");
 			break;
-		case '2': cout << "0010";
+		case '2': bin.append("0010");
 			break;
-		case '3': cout << "0011";
+		case '3': bin.append("0011");
 			break;
-		case '4': cout << "0100";
+		case '4': bin.append("0100");
 			break;
-		case '5': cout << "0101";
+		case '5': bin.append("0101");
 			break;
-		case '6': cout << "0110";
+		case '6': bin.append("0110");
 			break;
-		case '7': cout << "0111";
+		case '7': bin.append("0111");
 			break;
-		case '8': cout << "1000";
+		case '8': bin.append("1000");
 			break;
-		case '9': cout << "1001";
+		case '9': bin.append("1001");
 			break;
-		case 'A': cout << "1010";
+		case 'A': bin.append("1010");
 			break;
-		case 'B': cout << "1011";
+		case 'B': bin.append("1011");
 			break;
-		case 'C': cout << "1100";
+		case 'C': bin.append("1100");
 			break;
-		case 'D': cout << "1101";
+		case 'D': bin.append("1101");
 			break;
-		case 'E': cout << "1110";
+		case 'E': bin.append("1110");
 			break;
-		case 'F': cout << "1111";
+		case 'F': bin.append("1111");
 			break;
-		default: cout << "Invalid" << endl;
+		default:
+			break;
 		}
-		i++;
 
 	}
-	cout << endl;
+	return bin;
 
 }
